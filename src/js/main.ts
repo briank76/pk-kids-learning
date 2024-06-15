515726f356a3a8c4b7dc467aa9fe2f69f46fd456
briank76 <bk.atx.dev@gmail.com>
2024-06-15 12:51:49 -0500

    adjust gitignore and fix lint issues

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,10 +2,10 @@ import { SpellingReviewPage } from "./spelling-review/spelling-review.index";
 import "./spelling-review/components/completion-checklist/completion-checklist.component";
 
 document.addEventListener("DOMContentLoaded", () => {  
-    let myapp = document.getElementById('app') as HTMLDivElement;
+    const myapp = document.getElementById('app') as HTMLDivElement;
     if (myapp) {
         myapp.innerHTML = '';
-        let page = new SpellingReviewPage();
+        const page = new SpellingReviewPage();
         page.render(myapp);
     }
 });
