500eec759192b3acee35f3ce4642d598083c243d
briank76 <bk.atx.dev@gmail.com>
2024-11-17 14:19:52 -0600

    add routing and error page

---
+++ src/js/error-page/error-component.ts
@@ -0,0 +1,22 @@
+import { IPageComponent } from "../page.interface";
+import html from 'bundle-text:./error-component.html';
+import * as scssData from 'bundle-text:./error-component.scss';
+
+
+export class ErrorComponent implements IPageComponent {
+
+    public render(app: HTMLDivElement): void {
+        const content = this.getHtml(html);
+        const styles = document.createElement('style');
+        styles.textContent = scssData;
+        app.append(styles);
+        content.body.childNodes.forEach((x) => {
+            app.appendChild(x);
+        });
+    }
+
+    private getHtml(pageHtml: string): Document {
+        const content = new DOMParser().parseFromString(pageHtml, 'text/html');
+        return content;
+    }
+}
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,11 +1,34 @@
-import { SpellingReviewPage } from "./spelling-review/spelling-review.index";
+//import { SpellingReviewPage } from "./spelling-review/spelling-review.index";
+import { RoutingModule } from "./routing/routing";
 import "./spelling-review/components/completion-checklist/completion-checklist.component";
 
 document.addEventListener("DOMContentLoaded", () => {  
     const myapp = document.getElementById('app') as HTMLDivElement;
-    if (myapp) {
-        myapp.innerHTML = '';
+    /*if (myapp) {
+        myapp.innerText = '';
         const page = new SpellingReviewPage();
         page.render(myapp);
-    }
+    }*/
+
+    const routing = new RoutingModule();
+    routing.getRouteSubject.subscribe({
+        next: (value) => {
+            console.log(value);
+            const key = 'component';
+            const cmp = value[key];
+            const page = new cmp();
+            myapp.innerHTML = "";
+            page.render(myapp);
+            console.log('next');
+        },
+        error: (value: unknown) => {
+            console.log(value);
+            console.log('error');
+        },
+        complete: () => {
+            console.log('complete');
+        }
+    });
+
+    routing.initRouting();
 });
